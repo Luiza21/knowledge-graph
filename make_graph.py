b1112f37b0a1db651b153a3519f905b09aef266a
Luiza21 <grigoryan.luiza21@gmail.com>
2025-04-21 01:00:33 +0400

    good chunk size

--- make_graph.py
+++ make_graph.py
@@ -2,21 +2,20 @@ from neo4j import GraphDatabase
 from transformers import pipeline
 import spacy, coreferee
 from transformers import AutoTokenizer
+import pprint
 
-username="neo4j"
-password="password"
-url="bolt://localhost:7687"
+username = "neo4j"
+password = "password"
+url = "bolt://localhost:7687"
 
-driver = GraphDatabase.driver(
-    url, auth=(username, password)
-)
-
+driver = GraphDatabase.driver(url, auth=(username, password))
 
 
-coref_nlp = spacy.load('en_core_web_lg')
-coref_nlp.add_pipe('coreferee')
+coref_nlp = spacy.load("en_core_web_lg")
+coref_nlp.add_pipe("coreferee")
 
 tokenizer = AutoTokenizer.from_pretrained("Babelscape/rebel-large")
+
 
 def coref_text(text):
     coref_doc = coref_nlp(text)
@@ -27,9 +26,11 @@ def coref_text(text):
         if repres:
             resolved_text += " " + " and ".join(
                 [
-                    t.text
-                    if t.ent_type_ == ""
-                    else [e.text for e in coref_doc.ents if t in e][0]
+                    (
+                        t.text
+                        if t.ent_type_ == ""
+                        else [e.text for e in coref_doc.ents if t in e][0]
+                    )
                     for t in repres
                 ]
             )
@@ -39,38 +40,53 @@ def coref_text(text):
     return resolved_text
 
 
-
-
 def extract_triplets(text):
     triplets = []
-    relation, subject, relation, object_ = '', '', '', ''
+    relation, subject, relation, object_ = "", "", "", ""
     text = text.strip()
-    current = 'x'
-    for token in text.replace("<s>", "").replace("<pad>", "").replace("</s>", "").split():
+    current = "x"
+    for token in (
+        text.replace("<s>", "").replace("<pad>", "").replace("</s>", "").split()
+    ):
         if token == "<triplet>":
-            current = 't'
-            if relation != '':
-                triplets.append({'head': subject.strip(), 'type': relation.strip(),'tail': object_.strip()})
-                relation = ''
-            subject = ''
+            current = "t"
+            if relation != "":
+                triplets.append(
+                    {
+                        "head": subject.strip(),
+                        "type": relation.strip(),
+                        "tail": object_.strip(),
+                    }
+                )
+                relation = ""
+            subject = ""
         elif token == "<subj>":
-            current = 's'
-            if relation != '':
-                triplets.append({'head': subject.strip(), 'type': relation.strip(),'tail': object_.strip()})
-            object_ = ''
+            current = "s"
+            if relation != "":
+                triplets.append(
+                    {
+                        "head": subject.strip(),
+                        "type": relation.strip(),
+                        "tail": object_.strip(),
+                    }
+                )
+            object_ = ""
         elif token == "<obj>":
-            current = 'o'
-            relation = ''
+            current = "o"
+            relation = ""
         else:
-            if current == 't':
-                subject += ' ' + token
-            elif current == 's':
-                object_ += ' ' + token
-            elif current == 'o':
-                relation += ' ' + token
-    if subject != '' and relation != '' and object_ != '':
-        triplets.append({'head': subject.strip(), 'type': relation.strip(),'tail': object_.strip()})
+            if current == "t":
+                subject += " " + token
+            elif current == "s":
+                object_ += " " + token
+            elif current == "o":
+                relation += " " + token
+    if subject != "" and relation != "" and object_ != "":
+        triplets.append(
+            {"head": subject.strip(), "type": relation.strip(), "tail": object_.strip()}
+        )
     return triplets
+
 
 def save_triplets_to_neo4j(driver, triplets):
     with driver.session() as session:
@@ -91,49 +107,58 @@ def save_triplets_to_neo4j(driver, triplets):
                 """
                 session.run(cypher, head=head, relation=relation, tail=tail)
 
-def split_text_into_chunks(text, max_tokens):
-    doc = coref_nlp(text)
+
+def split_text(text, tokenizer, max_tokens=600, stride=200):
+    tokens = tokenizer(text, return_tensors="pt", truncation=False)["input_ids"][0]
     chunks = []
-    current = ""
-
-    for sent in doc.sents:
-        tokens = tokenizer(current + " " + sent.text, return_tensors="pt").input_ids.shape[1]
-        if tokens < max_tokens:
-            current += " " + sent.text
-        else:
-            chunks.append(current.strip())
-            current = sent.text
-
-    if current:
-        chunks.append(current.strip())
-
+    start = 0
+    while start < len(tokens):
+        end = start + max_tokens
+        chunk_tokens = tokens[start:end]
+        chunk_text = tokenizer.decode(chunk_tokens, skip_special_tokens=True)
+        chunks.append(chunk_text)
+        if end >= len(tokens):
+            break
+        start += max_tokens - stride  # overlap
     return chunks
+
 
 def dedup_triplets(triplets):
 
-    unique = list({(item['head'], item['type'], item['tail']) for item in triplets})
-    deduped = [{'head': h, 'type': t, 'tail': tail} for (h, t, tail) in unique]
+    unique = list({(item["head"], item["type"], item["tail"]) for item in triplets})
+    deduped = [{"head": h, "type": t, "tail": tail} for (h, t, tail) in unique]
 
     return deduped
 
+
 if __name__ == "__main__":
-    with open("input.txt") as file:
+    with open("input2.txt") as file:
         text = file.read()
 
     # resolve he / she to objects with spacy lib 'en_core_web_lg' model
     text_corefed = coref_text(text)
 
-    triplet_extractor = pipeline('text2text-generation', model='Babelscape/rebel-large', tokenizer='Babelscape/rebel-large')
+    triplet_extractor = pipeline(
+        "text2text-generation",
+        model="Babelscape/rebel-large",
+        tokenizer="Babelscape/rebel-large",
+    )
 
     triplets = []
-    for chunk in split_text_into_chunks(text_corefed, 500):
-        chunk += "List all relationships between people, animals and other objects, in the following text:"
-        extracted_text = triplet_extractor.tokenizer.batch_decode([triplet_extractor(chunk, return_tensors=True, return_text=False)[0]["generated_token_ids"]])
+    tokenizer = AutoTokenizer.from_pretrained("Babelscape/rebel-large")
+    for chunk in split_text(text_corefed, tokenizer):
+        extracted_text = triplet_extractor.tokenizer.batch_decode(
+            [
+                triplet_extractor(chunk, return_tensors=True, return_text=False)[0][
+                    "generated_token_ids"
+                ]
+            ]
+        )
         triplet = extract_triplets(extracted_text[0])
         triplets += triplet
-        print(chunk)
-        print(triplet)
-        print("------")
-    
-    save_triplets_to_neo4j(driver, dedup_triplets(triplets))
-    
+
+    triplets = dedup_triplets(triplets)
+    pprint.pprint(triplets)
+
+    # Save triplets to Neo4j
+    save_triplets_to_neo4j(driver, triplets)
--- make_graph.sh
+++ make_graph.sh
@@ -1,13 +1,12 @@
 # Run this once for installation
 
 # curl -LsSf https://astral.sh/uv/install.sh | sh
-# uv venv .venv --python=3.9 & source .venv/bin/activate
-# uv python install 3.9
-# uv pip install -U pip
-# uv pip install -r requirements.txt --prerelease=allow
+uv venv .venv --python=3.10 & source .venv/bin/activate
+uv python install 3.10
+uv pip install -r requirements.txt --prerelease=allow
 
-# python -m spacy download en_core_web_lg
-# python -m coreferee install en
+python -m spacy download en_core_web_lg
+python -m coreferee install en
 
 
 # Run this to create graph
