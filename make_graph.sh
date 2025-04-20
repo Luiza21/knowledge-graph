ef32359b164c563a61417b10158e4a74dd4c25ca
Luiza21 <grigoryan.luiza21@gmail.com>
2025-04-21 00:23:41 +0400

    Zuzo has owner now!

--- make_graph.sh
+++ make_graph.sh
@@ -1,14 +1,14 @@
 # Run this once for installation
 
 # curl -LsSf https://astral.sh/uv/install.sh | sh
-# source $HOME/.local/bin/env
-# uv venv
-# source .venv/bin/activate
-# uv pip install -r requirements.txt
+# uv venv .venv --python=3.9 & source .venv/bin/activate
+# uv python install 3.9
+# uv pip install -U pip
+# uv pip install -r requirements.txt --prerelease=allow
 
-# python3 -m spacy download en_core_web_lg
-# python3 -m coreferee install en
+# python -m spacy download en_core_web_lg
+# python -m coreferee install en
 
 
 # Run this to create graph
-python3 make_graph.py
+uv run --python 3.10 make_graph.py
